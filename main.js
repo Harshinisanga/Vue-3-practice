7747ab0440654ed4c998a027f259be774c41f9ea
Harshini Sanga <harshinisanga98@gmail.com>
2024-03-06 17:54:57 -0500

    L10 challenge

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id){
+            this.cart.push(id)
+        },
+        removeCart(id){
+           // this.cart.pop(id) //method 1
+           //method 2
+           const index = this.cart.indexOf(id);
+           if(index > -1){
+            this.cart.splice(index, 1)
+           }
+        }
+    }
 })
